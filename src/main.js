811419c1fda8bb9e6e6febde02fabedc7e1179a6
Igory23 <mustyacz@bk.ru>
2022-12-27 16:46:06 +0300

    На проверку

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { render } from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
 
+
 const siteMainElement = document.querySelector('.page-body');
 const siteHeaderElement = siteMainElement.querySelector('.trip-controls__filters');
 const boardPresenter = new BoardPresenter({ boardContainer: siteMainElement });
